286ffa2257aae9d2c15b0d282145ae40dcff66a7
Mark Harfouche <mark.harfouche@gmail.com>
2021-01-11 23:49:04 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,6 @@ cp $RECIPE_DIR/imagecodecs_distributor_setup.py .
 # Need to add the openjpeg2 cflags
 export CFLAGS="${CFLAGS} $(pkg-config --cflags libopenjp2)"
 
+# Build in parallel otherwise pypy + aarch doesn't complete in time
+$PYTHON setup.py build_ext -j ${CPU_COUNT}
 $PYTHON -m pip install . -vv
